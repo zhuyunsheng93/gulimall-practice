970dee918819a7dfec7ec647cddfad76718c320c
朱运升 <yunsheng.zhu@71360.com>
2021-07-22 16:53:16 +0800

    添加@EnableDiscoveryClient注解

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -3,8 +3,10 @@ package com.atguigu.gulimall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.order.dao")
 public class GulimallOrderApplication {
 
