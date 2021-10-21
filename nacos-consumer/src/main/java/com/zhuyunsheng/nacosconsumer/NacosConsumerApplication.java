f624b36d5c61da76ca0f3dfc5062a5b6cd7dd60b
朱运升 <yunsheng.zhu@71360.com>
2021-10-21 15:16:50 +0800

    add module  nacos-provider

---
+++ nacos-consumer/src/main/java/com/zhuyunsheng/nacosconsumer/NacosConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.zhuyunsheng.nacosconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class NacosConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+
+}
---
+++ nacos-provider/src/main/java/com/zhuyunsheng/nacospractice/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.zhuyunsheng.nacospractice.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  *@Description          
+ *  *@Author  zhuyunsheng       //作者
+ *  *@Date 2021/10/20 15:37      //创建时间
+ **/
+@RestController
+@RequestMapping()
+@RefreshScope
+public class HelloController {
+}
